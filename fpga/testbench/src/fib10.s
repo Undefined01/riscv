4df6eb24b3d05c483de9ba6924b847844d8ab61e
Undefined01 <amoscr@163.com>
2020-11-19 13:07:01 +0800

    test(fpga/core): 增加斐波那契测试，增加assert宏用以自动判定结果

---
+++ fpga/testbench/src/fib10.s
@@ -0,0 +1,48 @@
+# 只用加法计算斐波那契数列第十项
+# 最终x2应该等于
+
+li x1, 1		# 第零项
+li x2, 1		# 第一项
+
+add x3, x1, x2	# 第二项
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3	# 第三项
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+add x3, x1, x3
+mv x1, x2
+mv x2, x3
+
+nop
+nop
+nop
+nop
+nop
+nop

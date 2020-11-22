8c4286a9e33fd76e6d40b4414dec45a063177621
Undefined01 <amoscr@163.com>
2020-11-22 14:59:54 +0800

    test(fpga/test): 重构测试模块。无需手动指定执行步长，能够自动识别程序结束并进行断言。
    在common.h中定义了_halt标识符，其中定义了一个魔数用以退出程序执行（仅限testbench）。

--- fpga/testbench/src/common.h
+++ fpga/testbench/src/common.h
@@ -1,12 +1,16 @@
 .section text_end
 
 # 在代码段后增加多个指令，保证流水线执行完毕
+.globl _halt
+_halt:
 nop
 nop
 nop
 nop
 nop
 nop
+# Magic number，用以识别结束
+.word 0x7f2a214b
 
 # include 位置应该是开头，设置entry和_start符号
 .section entry
